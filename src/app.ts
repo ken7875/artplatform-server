5047e4506129b68a038b1ca94a1fc1eb11d33101
ken7875 <ken19970723@gmail.com>
2022-12-12 18:08:55 +0800

    add cors

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,11 @@ import express from 'express'
 import router from './routes/index'
 
 const app = express()
+const cors = require('cors');
 
 app.use('/api/artplatform', router)
+
+app.use(cors());
+app.options('*', cors());
 
 export default app
