c88cccd7db2a290608ce5f0b7ccc6ff0325f0b85
ken7875 <ken19970723@gmail.com>
2022-12-13 10:54:15 +0800

    更改 ranking data img路徑

--- src/dev-data/productsData.ts
+++ src/dev-data/productsData.ts
@@ -9,42 +9,42 @@ interface RankingData {
 export const rankingData: RankingData[] = [
     {
       id: 1,
-      img: '/_nuxt/assets/img/art04.jpg',
+      img: 'art04.jpg',
       title: '散步遇到的女人',
       auth: 'Zeo Jiang',
       price: 300
     },
     {
       id: 2,
-      img: '/_nuxt/assets/img/art05.jpg',
+      img: 'art05.jpg',
       title: '爆炸頭的母親',
       auth: 'Yui',
       price: 300
     },
     {
       id: 3,
-      img: '/_nuxt/assets/img/art06.jpg',
+      img: 'art06.jpg',
       title: '夏天的時候',
       auth: 'Bug Lin',
       price: 300
     },
     {
       id: 4,
-      img: '/_nuxt/assets/img/art07.jpg',
+      img: 'art07.jpg',
       title: '爆炸頭的母親',
       auth: 'Yui',
       price: 300
     },
     {
       id: 5,
-      img: '/_nuxt/assets/img/art08.jpg',
+      img: 'art08.jpg',
       title: '爆炸頭的母親',
       auth: 'Yui',
       price: 300
     },
     {
       id: 6,
-      img: '/_nuxt/assets/img/art09.jpg',
+      img: 'art09.jpg',
       title: '爆炸頭的母親',
       auth: 'Yui',
       price: 300
