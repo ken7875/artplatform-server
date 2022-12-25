34a513f9301cb781736499de3d2416c60fad0bae
ken7875 <ken19970723@gmail.com>
2022-12-25 17:13:55 +0800

    更改newestData資料

--- src/dev-data/productsData.ts
+++ src/dev-data/productsData.ts
@@ -50,43 +50,63 @@ export const rankingData: RankingData[] = [
       price: 300
     }
 ]
-type newestData = Omit<RankingData, 'auth'>
+type NewestData = Omit<RankingData, 'auth'> & { description: string }
 
-export const newestData: newestData[] = [
+export const newestData: NewestData[] = [
   {
     id: 1,
-    img: 'art04',
-    title: '散步遇到的女人',
-    price: 300
+    img: 'art09',
+    title: '區塊戀-他的私生活',
+    price: 300,
+    description: '唯一想擁有的人，唯一不可以擁有的'
   },
   {
     id: 2,
-    img: 'art05',
-    title: '爆炸頭的母親',
-    price: 300
+    img: 'art10',
+    title: '夏娃的醜聞',
+    price: 500,
+    description: '唯一想擁有的人，唯一不可以擁有的'
   },
   {
     id: 3,
-    img: 'art06',
-    title: '夏天的時候',
-    price: 300
+    img: 'art02',
+    title: '藍色啤酒海',
+    price: 600,
+    description: '唯一想擁有的人，唯一不可以擁有的'
   },
   {
     id: 4,
-    img: 'art07',
-    title: '爆炸頭的母親',
-    price: 300
+    img: 'art12',
+    title: '沒有人愛我',
+    price: 800,
+    description: '唯一想擁有的人，唯一不可以擁有的'
   },
   {
     id: 5,
-    img: 'art08',
-    title: '爆炸頭的母親',
-    price: 300
+    img: 'art13',
+    title: '不哭不哭眼淚是珍珠',
+    price: 900,
+    description: '唯一想擁有的人，唯一不可以擁有的'
   },
   {
     id: 6,
-    img: 'art09',
-    title: '爆炸頭的母親',
-    price: 300
+    img: 'art03',
+    title: '十分鐘就到',
+    price: 1200,
+    description: '唯一想擁有的人，唯一不可以擁有的'
+  },
+  {
+    id: 6,
+    img: 'art04',
+    title: '我這樣有美嗎',
+    price: 500,
+    description: '唯一想擁有的人，唯一不可以擁有的'
+  },
+  {
+    id: 6,
+    img: 'art27',
+    title: '我真的受傷了',
+    price: 700,
+    description: '唯一想擁有的人，唯一不可以擁有的'
   }
 ]
